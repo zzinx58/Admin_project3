532202b449288eee006572cfb7bf0425115707b4
zzx58 <q1095568627@163.com>
2023-12-14 11:59:02 +0800

    update: update BE api response type.

--- types/api_map_types/index.ts
+++ types/api_map_types/index.ts
@@ -22,9 +22,11 @@ export type API_Payload_POST_Login = {
 };
 export type API_Response_Raw_POST_Login = {
   code: number;
-  data: {
-    token: string;
-  };
+  data:
+    | {
+        token: string;
+      }
+    | undefined;
 } & Response_Raw_Error_CommonType;
 // ----------------------------------------------------------------
 export type API_Response_Raw_POST_Logout = {
@@ -34,17 +36,19 @@ export type API_Response_Raw_POST_Logout = {
 // ----------------------------------------------------------------
 export type API_Response_Raw_GET_getAdminInfo = {
   code: number;
-  data: {
-    id: number;
-    username: string;
-    phone: string | null;
-    email: string | null;
-    admin_type: Admin_Type_CommonType;
-    roles: string[];
-    deleted: boolean;
-    inserted_at: string;
-    updated_at: string;
-  };
+  data:
+    | {
+        id: number;
+        username: string;
+        phone: string | null;
+        email: string | null;
+        admin_type: Admin_Type_CommonType;
+        roles: string[];
+        deleted: boolean;
+        inserted_at: string;
+        updated_at: string;
+      }
+    | undefined;
 } & Response_Raw_Error_CommonType;
 // ----------------------------------------------------------------
 export type API_Payload_POST_createAdmin = {
@@ -56,35 +60,39 @@ export type API_Payload_POST_createAdmin = {
 };
 export type API_Response_Raw_POST_createAdmin = {
   code: number;
-  data: {
-    id: number;
-    username: string;
-    phone: string;
-    email: string;
-    admin_type: Admin_Type_CommonType;
-    roles: string[];
-    deleted: boolean;
-    inserted_at: string;
-    updated_at: string;
-  };
+  data:
+    | {
+        id: number;
+        username: string;
+        phone: string;
+        email: string;
+        admin_type: Admin_Type_CommonType;
+        roles: string[];
+        deleted: boolean;
+        inserted_at: string;
+        updated_at: string;
+      }
+    | undefined;
 } & Response_Raw_Error_CommonType;
 // ----------------------------------------------------------------
 export type API_Response_Raw_POST_getAdminList = {
   code: number;
-  data: {
-    total: number;
-    items: {
-      id: number;
-      username: string;
-      phone: string;
-      email: string;
-      admin_type: Admin_Type_CommonType;
-      roles: string[];
-      deleted: boolean;
-      inserted_at: string;
-      updated_at: string;
-    }[];
-  };
+  data:
+    | {
+        total: number;
+        items: {
+          id: number;
+          username: string;
+          phone: string;
+          email: string;
+          admin_type: Admin_Type_CommonType;
+          roles: string[];
+          deleted: boolean;
+          inserted_at: string;
+          updated_at: string;
+        }[];
+      }
+    | undefined;
 } & Response_Raw_Error_CommonType;
 
 // ----------------------------------------------------------------
@@ -92,34 +100,36 @@ export type API_Response_Raw_POST_getAdminList = {
 // ----------------------------------------------------------------
 export type API_Response_Raw_POST_getUserList = {
   code: number;
-  data: {
-    total: number;
-    items: {
-      type: User_Type_CommonType;
-      level: number; // 用户等级
-      signature: string; // 用户签名
-      location: {
-        city_name: string;
-        country_name: string;
-        region_name: string;
-      };
-      uid: number;
-      username: string;
-      nickname: string;
-      deleted: boolean;
-      inserted_at: string;
-      updated_at: string;
-      email: string;
-      channel: string; // 下载渠道
-      background: string; // 背景图片 url
-      phone: string;
-      banned: boolean; // 账号是否被禁用
-      gender: string;
-      flag_id: number; // 旗帜 ID
-      recover_effects_id: number; // 复原特效 ID
-      enter_effects_id: number; // 进入特效 ID
-      avatar_box_id: number; // 头像框 ID
-      avatar: string; // 头像 url
-    }[];
-  };
+  data:
+    | {
+        total: number;
+        items: {
+          type: User_Type_CommonType;
+          level: number; // 用户等级
+          signature: string; // 用户签名
+          location: {
+            city_name: string;
+            country_name: string;
+            region_name: string;
+          };
+          uid: number;
+          username: string;
+          nickname: string;
+          deleted: boolean;
+          inserted_at: string;
+          updated_at: string;
+          email: string;
+          channel: string; // 下载渠道
+          background: string; // 背景图片 url
+          phone: string;
+          banned: boolean; // 账号是否被禁用
+          gender: string;
+          flag_id: number; // 旗帜 ID
+          recover_effects_id: number; // 复原特效 ID
+          enter_effects_id: number; // 进入特效 ID
+          avatar_box_id: number; // 头像框 ID
+          avatar: string; // 头像 url
+        }[];
+      }
+    | undefined;
 } & Response_Raw_Error_CommonType;
