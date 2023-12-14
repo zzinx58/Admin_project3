4f88eb83e3cc30e104b37f538fded0224017ea02
zzx58 <q1095568627@163.com>
2023-12-14 16:58:32 +0800

    Test&chore: Test: project_constants_display
    style-display.vue
     layouts.
     tests folder path change.

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -2,7 +2,7 @@
 
 <template>
   <!-- <div class="bg-#F1F2FD w-screen h-screen grid place-items-center"> -->
-  <div class="bg-#F1F2FD w-screen h-screen grid place-content-center">
+  <div class="bg-main-base-header w-screen h-screen grid place-content-center">
     <slot />
   </div>
 </template>
